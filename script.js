799d4aa436065190328104e8a74e12ae3a56a5a1
w3school-test <mingcheng357@gmail.com>
2021-11-12 21:22:55 +0700

    setup

---
+++ script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//   // magnific-popup
+//   $('.parent-container').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     gallery: {
+//       enabled: true,
+//     },
+//   });
+
+//   $(document).scroll(function () {
+//     let pos = $(this).scrollTop();
+//     if (pos > 717) {
+//       $('#back-to-top').addClass('scrollTop');
+//     } else {
+//       $('#back-to-top').removeClass('scrollTop');
+//     }
+//   });
+// });
